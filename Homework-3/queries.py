e172fa3b336e69c7017f3c8e88190e926a0fa828
colinh09 <colin.hwang@cooper.edu>
2022-12-04 14:49:38 -0500

    finsihed

--- Homework-3/queries.py
+++ Homework-3/queries.py
@@ -11,7 +11,7 @@ answer = db.units.find_one(sort=[("placement", pymongo.DESCENDING)])
 print("The unit with the highest average placement is " + str(answer["unit"]) + " with an average placement of " + str(answer["placement"]) + "\n")
 
 # this will list really good augments
-print("Query 2: List augments that have an average placement below 4.5 and a top 4 percentage of above 50%.")
+print("Query 2: List augments that have an average placement below 4 and a top 4 percentage of above 60%.")
 answer = db.augments.find({"placement": {"$lt": 4}, "top4%": {"$gt": 60}})
 for i in answer:
     print(i.get("augment"))
@@ -54,7 +54,7 @@ for i in answer:
     print(i.get("augment") + " has been chosen " + str(int(i.get("freqency"))) + " times.")
 print("\n")
 
-# the same as query 6 but in augment form
+# these augments will be augments that are very niche/siutational, but strong in the right siutations
 print("Query 6: Pick the 20 least frequently used augments. Of that list, find the top 5 augments with the highest win rate.")
 
 answer = db.augments.aggregate([
@@ -71,3 +71,28 @@ answer = db.augments.aggregate([
 for i in answer:
     print(i.get("augment"))
 print("\n")
+
+# this is a question that I would ask myself when trying to find good augments and what units would be good fit for it
+print("Query 7: Get the units that best use the augment 'Better Together'. Average all the placement average of the augments and its top units.")
+
+answer = db.augments.find({"augment": "Better Together"})
+
+for i in answer:
+    unit_list = i.get("top units")
+    augment = i.get("placement")
+print("The top units are: " + unit_list["unit 0"] + " " + unit_list["unit 1"] + " "  + unit_list["unit 2"])
+
+answer2 = db.units.find({"unit": unit_list["unit 0"]})
+answer3 = db.units.find({"unit": unit_list["unit 1"]})
+answer4 = db.units.find({"unit": unit_list["unit 2"]})
+
+for i in answer2:
+    unit1 = i.get("placement")
+for i in answer3:
+    unit2 = i.get("placement")
+for i in answer4:
+    unit3 = i.get("placement")
+
+average = (unit1 + unit2 + unit3 + augment) / 4
+print("The average placement of the augment and its top units is: " + str(average))
+
--- Homework-3/scraper.py
+++ Homework-3/scraper.py
@@ -36,22 +36,54 @@ augment_stats_freq = []
 for element in soup.findAll(attrs={'class': 'flex items-center justify-end px-[14px] css-1ise070'}):
     augment_stats_freq.append(re.sub(r'[^0-9]', '', element.text))
 
-
 # get the win percentage of the augment (1st place is a win)
 augment_stats_win = []
 for element in soup.findAll(attrs={'class': 'flex items-center justify-end px-[14px] css-njtzbj'}):
     augment_stats_win.append(re.sub(r'[^0-9.]', '', element.text))
 
+# get the stat of the units that have the highest win rate with the augment
+# unfortunately, the maker of the website did not put the names of the units, only the source image
+# the maker of the website also called a lot of the units by a different name in the source images, so i had to map them all by hand
+# therefore, I have to do a (very very annoying) extra work to get the top units
+augment_stats_units = []
+for element in soup.findAll(attrs={'class': 'flex items-center pl-6 justify-center gap-[5px] px-[14px] css-175tob7'}):
+    for element2 in element.findAll('img'):
+        augment_stats_units.append(element2['src'])
+
+for i in range(len(augment_stats_units)):
+    augment_stats_units[i] = augment_stats_units[i][32:len(augment_stats_units[i]) - 13]
+    if (augment_stats_units[i] == 'dragongreen'):
+        augment_stats_units[i] = 'shi oh yu'
+    if (augment_stats_units[i] == 'dragonearth'):
+        augment_stats_units[i] = 'terra'
+    if (augment_stats_units[i] == 'dragonpurple'):
+        augment_stats_units[i] = 'syfen'
+    if (augment_stats_units[i] == 'dragonblue'):
+        augment_stats_units[i] = 'daeja'
+    if (augment_stats_units[i] == 'aquaticdragon'):
+        augment_stats_units[i] = 'sohm'
+    if (augment_stats_units[i] == 'dragongold'):
+        augment_stats_units[i] = 'idas'
+    if (augment_stats_units[i] == 'dragonguild'):
+        augment_stats_units[i] = 'zippy'
+    if (augment_stats_units[i] == 'dragonswain'):
+        augment_stats_units[i] = 'tyrant swain'
+    if (augment_stats_units[i] == '_volibear'):
+        augment_stats_units[i] = 'volibear'
+    if (augment_stats_units[i][0:len(augment_stats_units[i])-2] == 'nomsy'):
+        augment_stats_units[i] = 'nomsy'
+    augment_stats_units[i] = re.sub(r'[^a-zA-Z]', '', augment_stats_units[i]).lower()
 
 ### get data for all units ###
 driver.get('https://tactics.tools/units')
 content = driver.page_source
 soup = BeautifulSoup(content, 'html.parser')
-
+    
 # get all of the units' names
 champ_name = []
 for element in soup.findAll(attrs={'class': 'text-base font-medium whitespace-no-wrap truncate css-l6s31b'}):
-    champ_name.append(element.text)
+    champ_name.append(re.sub(r'[^a-zA-Z]', '', element.text).lower())
+
 
 # get the unit's playrate
 champ_playrate = []
@@ -69,6 +101,7 @@ client = MongoClient(conn_str)
 db = client.tft
 
 # this is super inefficent but I don't really want to think of another way since it works fine
+count = 0
 for i in range(len(augment_names)):
     augment_collection = {}
     augment_collection["augment"] = augment_names[i]
@@ -76,11 +109,26 @@ for i in range(len(augment_names)):
     augment_collection["top4%"] = float(augment_stats_top4[i])
     augment_collection["freqency"] = float(augment_stats_freq[i]) * 1000
     augment_collection["win%"] = float(augment_stats_win[i])
+    temp_nestedDict = {}
+    for i in range(3):
+        temp_nestedDict["unit " + str(i)] = augment_stats_units[count]
+        count += 1
+    augment_collection["top units"] = temp_nestedDict
     db.augments.insert_one(augment_collection)
 
 for i in range(len(champ_name)):
-    augment_collection = {}
-    augment_collection["unit"] = champ_name[i]
-    augment_collection["placement"] = float(champ_placement[i])
-    augment_collection["playrate"] = float(champ_playrate[i])
-    db.units.insert_one(augment_collection)
+    unit_collection = {}
+    unit_collection["unit"] = champ_name[i]
+    unit_collection["placement"] = float(champ_placement[i])
+    unit_collection["playrate"] = float(champ_playrate[i])
+    db.units.insert_one(unit_collection)
+
+# collection1 = db['augments']
+# cursor = collection1.find({})
+# for document in cursor:
+#     print(document)
+
+# collection1 = db['units']
+# cursor = collection1.find({})
+# for document in cursor:
+#     print(document)
